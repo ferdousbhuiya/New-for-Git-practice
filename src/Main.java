30d4bf71c0d74e27af8070ea3ca1972112a5bb6e
ferdousbhuiya <ferdousbhuiya@gmail.com>
2022-10-02 02:49:52 -0400

    Add main.java

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+
+    int a;
+    int b;
+    static int sum=0;
+    public static void  main(String[] args) {
+
+        addition(78, 98);{
+            System.out.println(sum);
+        }
+    }
+    public static int addition(int a, int b){
+        return sum =  a+b;
+    }
+    }
